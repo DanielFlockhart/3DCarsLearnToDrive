0d736000e78457c5eaddfd3dbf8d21e15bece13e
Daniel Flockhart <57047926+DanielFlockhart@users.noreply.github.com>
2021-04-12 20:45:35 +0100

    NetDisplay

--- Assets/Scripts/NetUI.cs
+++ Assets/Scripts/NetUI.cs
@@ -8,6 +8,7 @@ public class NetUI : MonoBehaviour
     public Color polarity;
     private Utils utilities;
 
+    public GameObject NetGraph;
     public GameObject Node;
 
     public float[] rgb;
@@ -21,7 +22,7 @@ public class NetUI : MonoBehaviour
     {
         
         utilities = FindObjectOfType<GameManager>().GetComponent<Utils>();
-        build(new int[4] {2,4,4,8});
+        build(new int[4] {2,4,3,6});
     }
 
 
@@ -33,7 +34,9 @@ public class NetUI : MonoBehaviour
     void build(int[] layers) {
         for (int x = 0; x < layers.Length; x++) {
             for (int y = 0; y < layers[x]; y++) {
-                addNode(x,y);
+                float xWidth = x;
+                float yHeight = y-(layers[x]/2);
+                addNode(xWidth, yHeight);
             }
         
         }
@@ -45,8 +48,9 @@ public class NetUI : MonoBehaviour
         rgb = utilities.mColour(weight);
         img.GetComponent<RawImage>().color = new Color(255* rgb[0], 255 * rgb[1], 255 * rgb[2], 255);
     }
-    void addNode(int xPos,int yPos) {
+    void addNode(float xPos,float yPos) {
         GameObject newNode = Instantiate(Node);
+        newNode.transform.SetParent(NetGraph.transform, false);
         weight = utilities.RandomFloat();
         setColour(newNode);
         newNode.transform.position = new Vector3(20*xPos,10*yPos,0);
