f01b264b384cf8a9692d34beb88c48e71116cbd4
Daniel Flockhart <57047926+DanielFlockhart@users.noreply.github.com>
2021-04-12 20:30:10 +0100

    Update NetUI.cs

--- Assets/Scripts/NetUI.cs
+++ Assets/Scripts/NetUI.cs
@@ -8,7 +8,7 @@ public class NetUI : MonoBehaviour
     public Color polarity;
     private Utils utilities;
 
-    private GameObject forwardNode;
+    public GameObject Node;
 
     public float[] rgb;
     public float weight;
@@ -19,9 +19,9 @@ public class NetUI : MonoBehaviour
 
     void Start()
     {
+        
         utilities = FindObjectOfType<GameManager>().GetComponent<Utils>();
-        weight = utilities.RandomFloat();
-        setColour(gameObject);
+        build(new int[4] {2,4,4,8});
     }
 
 
@@ -30,11 +30,25 @@ public class NetUI : MonoBehaviour
     {
 
     }
+    void build(int[] layers) {
+        for (int x = 0; x < layers.Length; x++) {
+            for (int y = 0; y < layers[x]; y++) {
+                addNode(x,y);
+            }
+        
+        }
+
+    }
 
     void setColour(GameObject img)
     {
-        
         rgb = utilities.mColour(weight);
-        img.GetComponent<RawImage>().color = new Color(255* rgb[0], 255 * rgb[1], 255 * rgb[2], 255);//rgb[0] * 255, rgb[1] * 255, rgb[2] * 255, 100
+        img.GetComponent<RawImage>().color = new Color(255* rgb[0], 255 * rgb[1], 255 * rgb[2], 255);
+    }
+    void addNode(int xPos,int yPos) {
+        GameObject newNode = Instantiate(Node);
+        weight = utilities.RandomFloat();
+        setColour(newNode);
+        newNode.transform.position = new Vector3(20*xPos,10*yPos,0);
     }
 }
